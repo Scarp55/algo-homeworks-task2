e0cd50e20676097d847610480b584b686fa273f7
Pivovarov Maksim <120288@mail.ru>
2022-07-14 17:44:37 +0600

    finish

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,14 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 9, 10, 14, 16, 19, 32, 32, 32, 56, 69, 72 };
+		int[] arr = { 14, 16, 19, 32, 32, 32, 56, 69, 72};
 
-		System.out.println(page(arr, 7) + " 7");
-//		System.out.println(page(arr, 14) + " 14");
-//		System.out.println(page(arr, 15) + " 15");
-//		System.out.println(page(arr, 16) + " 16");
-//		System.out.println(page(arr, 17) + " 17");
+		System.out.println(page(arr, 4) + " 4");
 		System.out.println(page(arr, 20) + " 20");
-//		System.out.println(page(arr, 32) + " 32");
-//		System.out.println(page(arr, 70) + " 70");
-//		System.out.println(page(arr, 72) + " 72");
-//		System.out.println(page(arr, 73) + " 73");
+		System.out.println(page(arr, 32) + " 32");
+		System.out.println(page(arr, 60) + " 60");
+		System.out.println(page(arr, 70) + " 70");
+		System.out.println(page(arr, 72) + " 72");
 
 	}
 
@@ -26,12 +22,22 @@ public class Main {
 		} else if (i >= arr[arr.length - 1]) {
 			return 0;
 		}
-
+		
 		int left = 0;
 		int right = arr.length - 1;
 
+		while (true) {
+			
+			int middle = (left + right) / 2;
+			if (i >= arr[middle] && i < arr[middle+1]) {
+				return arr.length - middle-1;
+			} else if (i < arr[middle]) {
+				right = middle-1;
+			} else if (i >= arr[middle]) {
+				left = middle+1;
+			}
+		}
 
-		return resalt;
 	}
 
 }
